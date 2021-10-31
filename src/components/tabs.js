2faea75032339eb000612df833f7d9d066004240
Zach Guo <3478203+zachguo@users.noreply.github.com>
2021-10-30 22:37:56 -0700

    add Tabs component

---
+++ src/components/tabs.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const Tabs = ({ children, defaultActiveTab }) => {
+  const [activeTab, setActiveTab] = useState(defaultActiveTab);
+  return (
+    <div>
+      <div className="tab-list">
+        {children.map(({ props: { tab, tabLabel } }) => (
+          <div
+            key={tab}
+            onClick={() => setActiveTab(tab)}
+            className={`tab-list-item ${tab === activeTab ? "tab-list-item-active" : ""}`}
+          >
+            {tabLabel ? tabLabel : tab}
+          </div>
+        ))}
+      </div>
+      <div>
+        {children.filter(({ props: { tab } }) => tab === activeTab)}
+      </div>
+    </div>
+  );
+};
+Tabs.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  defaultActiveTab: PropTypes.string.isRequired
+};
+
+const TabPane = ({ children, tab, tabLabel }) => {
+  return (
+    <div key={tab} label={tabLabel}>
+      {children}
+    </div>
+  );
+};
+TabPane.propTypes = {
+  children: PropTypes.element.isRequired,
+  tab: PropTypes.string.isRequired,
+  tabLabel: PropTypes.string.isRequired
+};
+
+export { Tabs, TabPane };
---
+++ src/stories/tabs.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Tabs, TabPane } from '../components/tabs';
+import "../index.css";
+import "./stories.css";
+
+export default {
+  title: 'Example/Tabs',
+  component: Tabs,
+  argTypes: {},
+  args: {}
+};
+
+const Template = (args) => (
+  <div>
+    <Tabs {...args} defaultActiveTab="tab-1">
+      <TabPane tab="tab-1" tabLabel="Tab 1">
+        <h3>Tab 1</h3>
+        <p>Content</p>
+      </TabPane>
+      <TabPane tab="tab-2" tabLabel="Tab 2">
+        <h3>Tab 2</h3>
+        <p>Content</p>
+      </TabPane>
+      <TabPane tab="tab-3" tabLabel="Tab 3">
+        <h3>Tab 3</h3>
+        <p>Content</p>
+      </TabPane>
+    </Tabs>
+  </div>
+);
+
+export const Default = Template.bind({});
